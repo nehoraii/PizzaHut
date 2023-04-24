c62185478cd22f7bb34326ff84b852d487af965e
nehoraii <nehoraii555@gmail.com>
2023-04-24 14:20:49 +0300

    add Server repository Entity and Vo

--- src/main/java/nehoraii/project/pizzahut/server/ConnectionPizzaAndPizzaToppingsServer.java
+++ src/main/java/nehoraii/project/pizzahut/server/ConnectionPizzaAndPizzaToppingsServer.java
@@ -2,9 +2,12 @@ package nehoraii.project.pizzahut.server;
 
 import nehoraii.project.pizzahut.entity.ConnectionPizzaAndPizzaToppingsEntity;
 import nehoraii.project.pizzahut.repository.ConnectionPizzaAndPizzaToppingsRepository;
+import nehoraii.project.pizzahut.vo.ConnectionPizzaAndPizzaToppingsVo;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Service;
+
+import java.util.NoSuchElementException;
 
 @Service
 public class ConnectionPizzaAndPizzaToppingsServer {
@@ -12,6 +15,34 @@ public class ConnectionPizzaAndPizzaToppingsServer {
     private ConnectionPizzaAndPizzaToppingsRepository connectionPizzaAndPizzaToppingsRepository;
     public ConnectionPizzaAndPizzaToppingsServer(ConnectionPizzaAndPizzaToppingsRepository connectionPizzaAndPizzaToppingsRepository){
         this.connectionPizzaAndPizzaToppingsRepository=connectionPizzaAndPizzaToppingsRepository;
+    }
+    public Long save(ConnectionPizzaAndPizzaToppingsVo Vo){
+        ConnectionPizzaAndPizzaToppingsEntity bean=new ConnectionPizzaAndPizzaToppingsEntity();
+        BeanUtils.copyProperties(Vo,bean);
+        bean=connectionPizzaAndPizzaToppingsRepository.save(bean);
+        return bean.getId();
+    }
+    public Long delete(Long id){
+        connectionPizzaAndPizzaToppingsRepository.deleteById(id);
+        return id;
+    }
+    public void update(Long id, ConnectionPizzaAndPizzaToppingsVo Vo){
+        ConnectionPizzaAndPizzaToppingsEntity bean=requireOne(id);
+        BeanUtils.copyProperties(Vo,bean);
+        connectionPizzaAndPizzaToppingsRepository.save(bean);
+    }
+    public ConnectionPizzaAndPizzaToppingsVo getById(Long id){
+        ConnectionPizzaAndPizzaToppingsEntity original=requireOne(id);
+        return toVO(original);
+    }
+    private ConnectionPizzaAndPizzaToppingsVo toVO(ConnectionPizzaAndPizzaToppingsEntity orginal){
+        ConnectionPizzaAndPizzaToppingsVo bean=new ConnectionPizzaAndPizzaToppingsVo();
+        BeanUtils.copyProperties(orginal,bean);
+        return bean;
+    }
+    private ConnectionPizzaAndPizzaToppingsEntity requireOne(Long id){
+        return connectionPizzaAndPizzaToppingsRepository.findById(id)
+                .orElseThrow(()-> new NoSuchElementException("Resource not found " + id));
     }
 
 }
